1f2f61e8c8c3ce3ab10b0f54fc5553a8a68ab59b
lare <74225315+laresamdeola@users.noreply.github.com>
2022-08-02 14:46:42 +0000

    a new simple program in Java.

--- Main4.java
+++ Main4.java
@@ -1,9 +1,10 @@
+/*
 class Main {
   public String name;
   public String state;
   public int age;
 
-  public Main(String yourName, String yourState, int yourAge){
+  public Main(String yourName, String yourState, int yourAge) {
     name = yourName;
     state = yourState;
     age = yourAge;
@@ -13,4 +14,4 @@ class Main {
     Main chika = new Main("Chikam", "Abia", 24);
     System.out.println(chika.name + " " + chika.state + " " + chika.age);
   }
-}
+}*/
