1eef5df93ad92c96c91065838f2a99e299109d54
lare <74225315+laresamdeola@users.noreply.github.com>
2022-08-07 18:15:48 +0000

    a simple calculator programme

---
+++ Calculator.java
@@ -0,0 +1,35 @@
+/*
+class Calculator {
+  public String operator;
+  public int firstNumber;
+  public int secondNumber;
+  public int answer;
+
+  public int addition(int firstNumber, int secondNumber){
+    answer = firstNumber + secondNumber;
+    return answer;
+  }
+
+  public int substraction(int firstNumber, int secondNumber){
+    answer = firstNumber - secondNumber;
+    if(answer < 0){
+      answer = 0;
+    };
+    return answer;
+  }
+
+  public int division(int firstNumber, int secondNumber){
+    answer = firstNumber / secondNumber;
+    return answer;
+  }
+
+  public int multiply(int firstNumber, int secondNumber){
+    answer = firstNumber * secondNumber;
+    return answer;
+  }
+
+  public static void calculator(String[] args){
+    System.out.println(addition(2, 3));
+  }
+}
+*/
--- Main.java
+++ Main.java
@@ -1,16 +1,33 @@
 class Main {
-  public String name;
-  public String season;
-  public Boolean seeds;
+  public static String operator;
+  public static int firstNumber;
+  public static int secondNumber;
+  public static int answer;
 
-  public Main(String fruitName, String fruitSeason, Boolean fruitSeeds){
-    name = fruitName;
-    season = fruitSeason;
-    seeds = fruitSeeds;
+  public static int addition(int firstNumber, int secondNumber){
+    answer = firstNumber + secondNumber;
+    return answer;
+  }
+
+  public static int substraction(int firstNumber, int secondNumber){
+    answer = firstNumber - secondNumber;
+    if(answer < 0){
+      answer = 0;
+    };
+    return answer;
+  }
+
+  public static int division(int firstNumber, int secondNumber){
+    answer = firstNumber / secondNumber;
+    return answer;
+  }
+
+  public static int multiply(int firstNumber, int secondNumber){
+    answer = firstNumber * secondNumber;
+    return answer;
   }
 
   public static void main(String[] args){
-    Main berries = new Main("Berries", "Autumn", true);
-    System.out.println(berries.name + " " + berries.season + " " + berries.seeds);
+    System.out.println(addition(2, 3) + "," + multiply(10, 50));
   }
 }
