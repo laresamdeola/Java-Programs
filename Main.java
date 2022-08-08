e7b16fe35ff153d3e0fb271487ad55cd26d8961c
lare <74225315+laresamdeola@users.noreply.github.com>
2022-08-08 12:32:22 +0000

    .

--- Main.java
+++ Main.java
@@ -4,30 +4,33 @@ class Main {
   public static int secondNumber;
   public static int answer;
 
-  public static int addition(int firstNumber, int secondNumber){
+  public static int addition(int firstNumber, int secondNumber) {
     answer = firstNumber + secondNumber;
     return answer;
   }
 
-  public static int substraction(int firstNumber, int secondNumber){
+  public static int substraction(int firstNumber, int secondNumber) {
     answer = firstNumber - secondNumber;
-    if(answer < 0){
+    if (answer < 0) {
       answer = 0;
-    };
+    }
+    if (answer = 0) {
+      answer = 0;
+    }
     return answer;
   }
 
-  public static int division(int firstNumber, int secondNumber){
+  public static int division(int firstNumber, int secondNumber) {
     answer = firstNumber / secondNumber;
     return answer;
   }
 
-  public static int multiply(int firstNumber, int secondNumber){
+  public static int multiply(int firstNumber, int secondNumber) {
     answer = firstNumber * secondNumber;
     return answer;
   }
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     System.out.println(addition(2, 3) + "," + multiply(10, 50));
   }
 }
