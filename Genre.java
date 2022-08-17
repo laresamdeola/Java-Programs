dd1ce23c00450aaf134ef7ea92758569122ca2a8
lare <74225315+laresamdeola@users.noreply.github.com>
2022-08-17 21:37:47 +0000

    .

--- Genre.java
+++ Genre.java
@@ -1,20 +1,19 @@
 class Genre {
-  String artistName, message;
-  String artistAlbum;
+  String artistName, artistAlbum, message;
   boolean isAlive;
 
-  public String nameOfArtist(){
+  public String nameOfArtist() {
     return artistName;
   }
 
-  public String nameOfAlbum(){
+  public String nameOfAlbum() {
     return artistAlbum;
   }
 
-  public String deadOrAlive(){
-    if(isAlive == false){
+  public String deadOrAlive() {
+    if (isAlive == false) {
       message = artistName + "" + " has passed away";
-    } else{
+    } else {
       message = artistName + "" + " is alive";
     }
     return message;
